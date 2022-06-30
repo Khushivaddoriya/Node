017776e7da0d41e926b3c4063de42ac44c245592
Khushi Vaddoriya <khushi.m.vaddoriya@gmail.com>
2022-06-30 16:48:09 +0530

    30-06

--- http/url_server.js
+++ http/url_server.js
@@ -8,14 +8,19 @@ const http = require("http");
 //http://localhost:8080/?uname=admin&upwd=admin
 const url = require("url");
 
+const fs=require('fs');
+
 //create the http server
+const a= fs.readFileSync("demo.txt","utf-8")
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   const q = url.parse(req.url, true).query;
   //   console.log(req.url);
   console.log(q);
   if (q.uname == "admin" && q.upwd == "admin") {
-    res.write("<h1>Login Success</h1>");
+    res.write(a)
+    // res.write("<h1>Login Success</h1>");
   } else {
     res.write("<h1>Login Fail</h1>");
   }
